607a9cb8762ce963d8fb3901c45ccbfbe475b5c0
KavithaGaneshh <Kavithaaguruu@gmail.com>
2025-10-28 17:13:04 +0530

    Update database.py

--- database.py
+++ database.py
@@ -3,7 +3,8 @@ import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql+asyncpg://user:password@host:port/tnvote_form"
+DATABASE_URL = postgresql+asyncpg://tamilnadu_voting_db_user:iTBLuVTj6eloL0CK8L91AXsf7Ny8Gktj@dpg-d409hmc9c44c738o4kjg-a:5432/tamilnadu_voting_db"
+
 
 engine = create_async_engine(DATABASE_URL, future=True, echo=False)
 AsyncSessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
@@ -12,3 +13,4 @@ Base = declarative_base()
 async def get_db():
     async with AsyncSessionLocal() as session:
         yield session
+
